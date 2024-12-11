958de8d132b86801276dd46b3dda05081166a5b2
GHuyHuynh <huy.huynh@dal.ca>
2024-12-11 18:56:30 -0400

    refactor

---
+++ extension/content.js
@@ -0,0 +1,8 @@
+// content.js
+document.addEventListener('DOMContentLoaded', () => {
+  // Wait for target element to load
+  const targetNode = document.querySelector('.css-106ak42');
+  if (targetNode) {
+    injectWalkthrough();
+  }
+});
