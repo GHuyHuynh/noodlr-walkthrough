2275b2f6f852659bdfe98eb0fc582d79d2a06648
GHuyHuynh <huy.huynh@dal.ca>
2024-12-21 17:14:48 -0400

    Refactor manifest for upload

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,6 +1,10 @@
+const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   mode: 'development',
-  entry: './src/test.tsx',
+  entry: path.resolve('src/app.tsx'),
   module: {
     rules: [
       {
@@ -10,10 +14,29 @@ module.exports = {
       }
     ],
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { 
+          from: path.resolve('src/assets/manifest.json'), 
+          to: path.resolve('dist')
+        },
+        { 
+          from: path.resolve('src/assets/icon.png'), 
+          to: path.resolve('dist/assets')
+        },
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Noodlr Walkthrough',
+      filename: 'index.html',
+    }),
+  ],
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
   },
   output: {
-    filename: 'index.js',
+    filename: 'contentScript.js',
+    path: path.resolve('dist'),
   }
 };
